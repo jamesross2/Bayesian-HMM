687c0285352b412eb8a09087e44351d8b176c8cc
jamesross2 <jamespatross@gmail.com>
2019-03-31 16:20:58 +1100

    Forces consistent linting on all future commits.
    * apply black linting to project
    
    * include black in Travis CI testing

--- setup.py
+++ setup.py
@@ -1,27 +1,22 @@
 import setuptools
 
 setuptools.setup(
-    name='bayesian_hmm',
-    version='0.0.0a0',
-    license='MIT',
-    packages=setuptools.find_packages(exclude=['tests']),
-    author='James Ross',
-    author_email='jamespatross@gmail.com',
-    description='A non-parametric Bayesian approach to Hidden Markov Models',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/jamesross2/Bayesian-HMM',
+    name="bayesian_hmm",
+    version="0.0.0a0",
+    license="MIT",
+    packages=setuptools.find_packages(exclude=["tests"]),
+    author="James Ross",
+    author_email="jamespatross@gmail.com",
+    description="A non-parametric Bayesian approach to Hidden Markov Models",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/jamesross2/Bayesian-HMM",
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent'],
-    install_requires=[
-        'scipy',
-        'numpy',
-        'pandas',
-        'terminaltables',
-        'tqdm',
-        'sympy'
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    test_suite='py.test',
-    tests_require=['pytest'])
+    install_requires=["scipy", "numpy", "pandas", "terminaltables", "tqdm", "sympy"],
+    test_suite="py.test",
+    tests_require=["pytest", "black"],
+)
