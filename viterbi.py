8abd35cd50387358c7feec59407fc78d46956243
James Hortle <jamesohortle@gmail.com>
2020-03-27 15:43:14 +1100

    Tidy up.

--- viterbi.py
+++ viterbi.py
@@ -3,18 +3,18 @@
 
 import numpy as np
 
-from bayesian_hmm.chain import Chain
-
-# For now, copy the example on Wikipedia.
-# Later, generalize as needed.
-
 
 def viterbi(observations, states, start_p, trans_p, emit_p) -> np.ndarray:
+    """Implement the Viterbi algorithm as outlined on [Wikipedia](https://en.wikipedia.org/wiki/Viterbi_algorithm) for now. Later, generalize as needed."""
+
+    # TODO: Think about making states and observations numerical for easier implementation below.
+    # I.e., removing the `enumerate()`s.
+
     # Pre-allocate matrices for T1, T2.
     T1 = np.empty((len(states), len(observations)), dtype=np.float)
     T2 = np.empty((len(states), len(observations)), dtype=np.float)
 
-    # Initialize initial states.
+    # Initialize states.
     for i, state in enumerate(states):
         T1[i, 0] = start_p[state] * emit_p[state][observations[i]]
         T2[i, 0] = 0.0
@@ -37,7 +37,7 @@ def viterbi(observations, states, start_p, trans_p, emit_p) -> np.ndarray:
 
     Z[-1] = np.argmax(T1[:, -1])
     X[-1] = states[int(Z[-1])]
-    for j in range(len(observations) - 1, 0, -1):
+    for j in range(len(observations) - 1, 0, -1):  # FIX: ugly use of `range()`.
         Z[j - 1] = T2[int(Z[j]), j]
         X[j - 1] = states[int(Z[j - 1])]
 
